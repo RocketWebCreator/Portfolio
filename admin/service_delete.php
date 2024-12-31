0f64d5279b0dec9034f16373e77444ccbc3262da
RocketWebCreator <zubairzakriya21@gmail.com>
2024-12-31 16:00:31 +0500

    Delete admin directory
    Signed-off-by: RocketWebCreator <zubairzakriya21@gmail.com>

--- admin/service_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once "db.php"; 
-$id = base64_decode($_GET['id']);
-$dbcon->query("DELETE FROM services WHERE id=$id");
-$_SESSION['service_delete'] = "Delete Successfully!";
-header('location: services.php');
-
-?>
